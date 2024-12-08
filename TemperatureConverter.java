931ee1da0472d7b42f06ab61ed1b358dedb92115
Els-sie <aelsie.atsu@gmail.com>
2024-12-08 05:51:07 +0000

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,19 +4,19 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        // Display options to the user
         System.out.println("Welcome to the Temperature Converter!");
         System.out.println("Please choose an option:");
         System.out.println("1. Convert Celsius to Fahrenheit");
         System.out.println("2. Convert Fahrenheit to Celsius");
 
-        
+        // Take user choice
         int choice = scanner.nextInt();
 
-    
+        // Variable to hold the input temperature and converted value
         double inputTemp, convertedTemp;
 
-    
+        // Perform the conversion based on user choice
         if (choice == 1) {
             System.out.print("Enter the temperature in Celsius: ");
             inputTemp = scanner.nextDouble();
@@ -31,7 +31,7 @@ public class TemperatureConverter {
             System.out.println("Invalid choice! Please restart the program and choose a valid option.");
         }
 
-        
+        // Close the scanner
         scanner.close();
     }
 }
